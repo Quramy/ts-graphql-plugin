61acd3eb9b895346c5a9609e55ada7f23243b4e0
y-kurami <yosuke.kurami@gmail.com>
2017-04-10 03:27:39 +0900

    make e2e tests green

--- e2e/specs/completions.js
+++ e2e/specs/completions.js
@@ -5,7 +5,7 @@ function run(server) {
   server.send({ command: 'completions', arguments: { file: 'main.ts', offset: 22, line: 1, prefix: '' } });
   return server.close().then(() => {
     assert.equal(server.responses.length, 2);
-    assert.equal(server.responses[1].body.length, 4);
+    assert.equal(server.responses[1].body.length, 3);
     assert(server.responses[1].body.some(item => item.name === 'hello'));
   });
 }
