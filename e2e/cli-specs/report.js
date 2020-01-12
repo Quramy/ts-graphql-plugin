032b19c92443cc493a175f89f32e79b023ad6b90
Yosuke Kurami <yosuke.kurami@gmail.com>
2020-01-12 21:33:42 +0900

    chore: use --includeFragments option

--- e2e/cli-specs/report.js
+++ e2e/cli-specs/report.js
@@ -6,7 +6,14 @@ const rimraf = require('rimraf');
 async function run(cli) {
   const reportPath = 'project-fixtures/react-apollo-prj/GRAPHQL_OPERATIONS.md';
   rimraf.sync(path.resolve(__dirname, '../..', reportPath));
-  const { code } = await cli.run('report', ['-p', 'project-fixtures/react-apollo-prj', '--verbose', '-o', reportPath]);
+  const { code } = await cli.run('report', [
+    '-p',
+    'project-fixtures/react-apollo-prj',
+    '--verbose',
+    '-o',
+    reportPath,
+    '--includeFragments',
+  ]);
   assert.equal(fs.existsSync(path.resolve(__dirname, '../../', reportPath)), true);
   assert.equal(code, 0);
 }
