2f543b2cd53b099c969bec3db75c2436adefa551
Yosuke Kurami <yosuke.kurami@gmail.com>
2020-02-03 00:55:55 +0900

    chore: add getSemanticDiagnostics test cases

--- src/graphql-language-service-adapter/get-semantic-diagonistics.test.ts
+++ src/graphql-language-service-adapter/get-semantic-diagonistics.test.ts
@@ -2,6 +2,8 @@ import * as ts from 'typescript/lib/tsserverlibrary';
 import { AdapterFixture } from './testing/adapter-fixture';
 import { createSimpleSchema } from './testing/simple-schema';
 import { GraphQLSchema } from 'graphql';
+import { mark, Markers } from '../string-util/testing/position-marker';
+import { ERROR_CODES } from '../errors';
 
 function craeteFixture(name: string, schema?: GraphQLSchema) {
   return new AdapterFixture(name, schema);
@@ -18,57 +20,80 @@ describe('getSemanticDiagnostics', () => {
     expect(actual).toEqual(defaultSemanticDigostics);
   });
 
-  it('should validate GraphQL syntax in template string', () => {
-    const fixture = craeteFixture('input.ts', createSimpleSchema());
-    const validateFn: () => ts.Diagnostic[] = fixture.adapter.getSemanticDiagnostics.bind(
-      fixture.adapter,
-      delegateFn,
-      'input.ts',
+  it('should report errors coused by schema building', () => {
+    const fixture = craeteFixture('input.ts');
+    const validateFn = fixture.adapter.getSemanticDiagnostics.bind(fixture.adapter, delegateFn, 'input.ts');
+    fixture.adapter.updateSchema(
+      [
+        {
+          fileName: 'schema.graphql',
+          message: 'some schema build error',
+          fileContent: '',
+          locations: [{ line: 0, character: 0 }],
+        },
+      ],
+      null,
     );
+    fixture.source = 'const query = `query { hello }`;';
+    const actual = validateFn();
+    expect(actual[0].code).toBe(ERROR_CODES.schemaBuildError.code);
+    expect(actual[0].messageText).toMatchSnapshot();
+  });
 
-    fixture.source = 'const ql = `';
-    const [actual1] = validateFn();
-    const messageText = actual1.messageText as string;
-    expect(messageText.match(/Syntax Error:/)).toBeTruthy();
-    expect(actual1.start).toBe(11); // start character
+  it('should return empty array for valid GraphQL template string', () => {
+    const fixture = craeteFixture('input.ts', createSimpleSchema());
+    const validateFn = fixture.adapter.getSemanticDiagnostics.bind(fixture.adapter, delegateFn, 'input.ts');
 
-    // prettier-ignore
-    fixture.source = 'const ql = ``';
-    expect(validateFn()[0].start).toBe(12);
+    fixture.source = `
+      const query = \`
+        query {
+          hello
+        }
+      \`;
+    `;
+    expect(validateFn()).toEqual([]);
+  });
 
-    // prettier-ignore
-    fixture.source = 'const ql = `' + '\n';
-    +'`';
-    expect(validateFn()[0].start).toBe(13);
+  it('should return syntax error with empty template literal', () => {
+    const fixture = craeteFixture('input.ts', createSimpleSchema());
+    const validateFn = fixture.adapter.getSemanticDiagnostics.bind(fixture.adapter, delegateFn, 'input.ts');
 
-    // prettier-ignore
-    fixture.source = 'const ql = `' + '\n'
-                   + '{`';
-    const [actual2] = validateFn();
-    expect(actual2.start).toBe(13); // start character (multiline)
+    fixture.source = `
+      const query = \`\`;
+    `;
+    const actual = validateFn();
+    expect(actual.length).toBe(1);
+    expect((actual[0].messageText as string).match(/Syntax Error:/)).toBeTruthy();
+  });
 
-    fixture.source = 'const ql = `query { hello }`';
-    expect(validateFn()).toEqual([]); // no errors for valid query
+  it('should return position of error token', () => {
+    const fixture = craeteFixture('input.ts', createSimpleSchema());
+    const validateFn = fixture.adapter.getSemanticDiagnostics.bind(fixture.adapter, delegateFn, 'input.ts');
 
-    // prettier-ignore
-    fixture.source = 'const ql = `query {' + '\n'
-                   + '  hello' + '\n'
-                   + ' }`';
-    expect(validateFn()).toEqual([]); // no errors for valid query (multiline)
+    const markers: Markers = {};
+
+    fixture.source = mark(
+      // prettier-ignore
+      '    const query = `query {`;    ' + '\n' +
+      '%%%                      ^   %%%' + '\n' +
+      '%%%                      a1  %%%' + '\n',
+      markers,
+    );
+    const actual = validateFn();
+    expect(actual[0].start).toBe(markers.a1.pos);
   });
 
   it('should return diagnostic array for invalid GraphQL template string', () => {
     const fixture = craeteFixture('input.ts', createSimpleSchema());
-    const validateFn: () => ts.Diagnostic[] = fixture.adapter.getSemanticDiagnostics.bind(
-      fixture.adapter,
-      delegateFn,
-      'input.ts',
-    );
+    const validateFn = fixture.adapter.getSemanticDiagnostics.bind(fixture.adapter, delegateFn, 'input.ts');
 
-    // prettier-ignore
-    fixture.source = 'const ql = `query {' + '\n'
-                   + '  hoge,' + '\n'
-                   + ' }`';
+    fixture.source = `
+      const query = \`
+        query {
+          hoge
+        }
+      \`;
+    `;
     const [actual] = validateFn();
     const messageText = actual.messageText as string;
     expect(/Cannot query field "hoge"/.test(messageText)).toBeTruthy();
@@ -76,49 +101,30 @@ describe('getSemanticDiagnostics', () => {
 
   it('should filter severity of GraphQL errors', () => {
     const fixture = craeteFixture('input.ts', createSimpleSchema());
-    const validateFn: () => ts.Diagnostic[] = fixture.adapter.getSemanticDiagnostics.bind(
-      fixture.adapter,
-      delegateFn,
-      'input.ts',
-    );
+    const validateFn = fixture.adapter.getSemanticDiagnostics.bind(fixture.adapter, delegateFn, 'input.ts');
 
-    // prettier-ignore
-    fixture.source = 'const ql = `query {' + '\n'
-                   + '  helloWorld,' + '\n'
-                   + ' }`';
-    const [actual1] = validateFn();
-    expect(actual1.category).toBe(ts.DiagnosticCategory.Warning);
+    fixture.source = `
+      const query = \`
+        query {
+          helloWorld
+        }
+      \`;
+    `;
+    expect(validateFn()[0].category).toBe(ts.DiagnosticCategory.Warning);
 
-    // prettier-ignore
-    fixture.source = 'const ql = `query {' + '\n'
-                   + '  helo,' + '\n'
-                   + ' }`';
-    const [actual2] = validateFn();
-    expect(actual2.category).toBe(ts.DiagnosticCategory.Error);
-  });
-
-  it('should return empty array for valid GraphQL template string', () => {
-    const fixture = craeteFixture('input.ts', createSimpleSchema());
-    const validateFn: () => ts.Diagnostic[] = fixture.adapter.getSemanticDiagnostics.bind(
-      fixture.adapter,
-      delegateFn,
-      'input.ts',
-    );
-
-    // prettier-ignore
-    fixture.source = 'const ql = `query {' + '\n'
-                   + '  hello' + '\n'
-                   + ' }`';
-    expect(validateFn()).toEqual([]); // no errors for valid query (multiline)
+    fixture.source = `
+      const query = \`
+        query {
+          helo
+        }
+      \`;
+    `;
+    expect(validateFn()[0].category).toBe(ts.DiagnosticCategory.Error);
   });
 
   it('should work with incomplete document', () => {
     const fixture = craeteFixture('input.ts', createSimpleSchema());
-    const validateFn: () => ts.Diagnostic[] = fixture.adapter.getSemanticDiagnostics.bind(
-      fixture.adapter,
-      delegateFn,
-      'input.ts',
-    );
+    const validateFn = fixture.adapter.getSemanticDiagnostics.bind(fixture.adapter, delegateFn, 'input.ts');
 
     fixture.source = `
       export const fragment = gql\`
@@ -128,5 +134,57 @@ describe('getSemanticDiagnostics', () => {
       \`;
     `;
     expect(validateFn().length).toBe(1);
+  });
+
+  it('should return "templateIsTooComplex" error when template node has too complex interpolation', () => {
+    const fixture = craeteFixture('input.ts', createSimpleSchema());
+    const validateFn = fixture.adapter.getSemanticDiagnostics.bind(fixture.adapter, delegateFn, 'input.ts');
+
+    const markers: Markers = {};
+    fixture.source = mark(
+      `
+      const query = \`
+        \${someFn()}
+   %%%  \\ ^          %%%
+   %%%  \\ a1         %%%
+        query {
+          ...MyFragment
+        }
+      \`;
+    `,
+      markers,
+    );
+    const actual = validateFn();
+    expect(actual[0].code).toBe(ERROR_CODES.templateIsTooComplex.code);
+    expect(actual[0].start).toBe(markers.a1.pos);
+  });
+
+  it('should return "isInOtherExpression" error when interpolated fragment has error', () => {
+    const fixture = craeteFixture('input.ts', createSimpleSchema());
+    const validateFn = fixture.adapter.getSemanticDiagnostics.bind(fixture.adapter, delegateFn, 'input.ts');
+
+    const markers: Markers = {};
+    fixture.source = mark(
+      `
+      const fragment = \`
+        fragment MyFragment on Query {
+          hoge
+        }
+      \`;
+      const query = \`
+        \${fragment}
+   %%%  \\ ^          %%%
+   %%%  \\ a1         %%%
+        query {
+          ...MyFragment
+        }
+      \`;
+    `,
+      markers,
+    );
+    const actual = validateFn();
+    expect(actual.length).toBe(2);
+    expect(actual[1].code).toBe(ERROR_CODES.errorInOtherInterpolation.code);
+    expect(actual[1].start).toBe(markers.a1.pos);
   });
 });
