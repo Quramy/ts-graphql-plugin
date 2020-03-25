dbc660334c9c0de40cb8bdf643b3614f062fe93b
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-03-25 11:33:54 +0100

    Fix a typo

--- src/schema-manager/schema-manager-factory.test.ts
+++ src/schema-manager/schema-manager-factory.test.ts
@@ -3,49 +3,49 @@ import { HttpSchemaManager } from './http-schema-manager';
 import { FileSchemaManager } from './file-schema-manager';
 import { createTestingSchemaManagerHost } from './testing/testing-schema-manager-host';
 
-describe('SchemaManagerFactory', () => {
+describe(SchemaManagerFactory, () => {
   it('should return HttpSchemaManager from http url string', () => {
-    const facotry = new SchemaManagerFactory(
+    const factory = new SchemaManagerFactory(
       createTestingSchemaManagerHost({
         schema: 'http://localhost',
       }),
     );
-    const actual = facotry.create();
+    const actual = factory.create();
     expect(actual instanceof HttpSchemaManager).toBeTruthy();
   });
 
   it('should return HttpSchemaManager from https url string', () => {
-    const facotry = new SchemaManagerFactory(
+    const factory = new SchemaManagerFactory(
       createTestingSchemaManagerHost({
         schema: 'https://localhost',
       }),
     );
-    const actual = facotry.create();
+    const actual = factory.create();
     expect(actual instanceof HttpSchemaManager).toBeTruthy();
   });
 
   it('should return FileSchemaManager from file schema string', () => {
-    const facotry = new SchemaManagerFactory(
+    const factory = new SchemaManagerFactory(
       createTestingSchemaManagerHost({
         schema: 'file:///tmp/s.json',
       }),
     );
-    const actual = facotry.create();
+    const actual = factory.create();
     expect(actual instanceof FileSchemaManager).toBeTruthy();
   });
 
   it('should return FileSchemaManager from no schema string', () => {
-    const facotry = new SchemaManagerFactory(
+    const factory = new SchemaManagerFactory(
       createTestingSchemaManagerHost({
         schema: '/tmp/s.json',
       }),
     );
-    const actual = facotry.create();
+    const actual = factory.create();
     expect(actual instanceof FileSchemaManager).toBeTruthy();
   });
 
   it('should return HttpSchemaManager from http object', () => {
-    const facotry = new SchemaManagerFactory(
+    const factory = new SchemaManagerFactory(
       createTestingSchemaManagerHost({
         schema: {
           http: {
@@ -54,12 +54,12 @@ describe('SchemaManagerFactory', () => {
         },
       }),
     );
-    const actual = facotry.create();
+    const actual = factory.create();
     expect(actual instanceof HttpSchemaManager).toBeTruthy();
   });
 
   it('should return FileSchemaManager from file object', () => {
-    const facotry = new SchemaManagerFactory(
+    const factory = new SchemaManagerFactory(
       createTestingSchemaManagerHost({
         schema: {
           file: {
@@ -68,7 +68,7 @@ describe('SchemaManagerFactory', () => {
         },
       }),
     );
-    const actual = facotry.create();
+    const actual = factory.create();
     expect(actual instanceof FileSchemaManager).toBeTruthy();
   });
 });
