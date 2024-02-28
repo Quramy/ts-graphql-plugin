8d08a1a047e25b00e29395ff694eda4a913865e4
Quramy <yosuke.kurami@gmail.com>
2024-02-29 06:16:17 +0900

    test: Add caes to fill branch of HttpSchemaManager

--- src/schema-manager/http-schema-manager.test.ts
+++ src/schema-manager/http-schema-manager.test.ts
@@ -19,57 +19,134 @@ describe(HttpSchemaManager, () => {
 
   afterAll(() => server.close());
 
-  it('should return null if request fail', async () => {
-    server.use(http.post('http://localhost/graphql', () => new Response(null, { status: 500 })));
-    const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
-    const manager = new HttpSchemaManager(schemaManagerHost, {
-      url: 'http://localhost/graphql',
+  describe(HttpSchemaManager.prototype.waitBaseSchema, () => {
+    it('should resolve null if HTTP request fail', async () => {
+      server.use(http.post('http://localhost/graphql', () => new Response(null, { status: 500 })));
+      const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
+      const manager = new HttpSchemaManager(schemaManagerHost, {
+        url: 'http://localhost/graphql',
+      });
+      const schema = await manager.waitBaseSchema();
+      expect(schema).toBeNull();
     });
-    const schema = await manager.waitBaseSchema();
-    expect(schema).toBeNull();
+
+    it('should resolve null if _getOptions retuns null', async () => {
+      class TestingHttpSchemaManager extends HttpSchemaManager {
+        _getOptions() {
+          return Promise.resolve(null);
+        }
+      }
+      server.use(schemaHandler);
+      const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
+      const manager = new TestingHttpSchemaManager(schemaManagerHost, {
+        url: 'http://localhost/graphql',
+      });
+      const schema = await manager.waitBaseSchema();
+      expect(schema).toBeNull();
+    });
+
+    it('should resolve schema', async () => {
+      server.use(schemaHandler);
+      const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
+      const manager = new HttpSchemaManager(schemaManagerHost, {
+        method: 'POST',
+        url: 'http://localhost/graphql',
+      });
+      const schema = await manager.waitBaseSchema();
+      expect(schema).toBeInstanceOf(GraphQLSchema);
+    });
   });
 
-  it('should get schema from GraphQL server', async () => {
-    server.use(schemaHandler);
-    const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
-    const manager = new HttpSchemaManager(schemaManagerHost, {
-      method: 'POST',
-      url: 'http://localhost/graphql',
-    });
-    const lazySchema = new Promise(res => manager.registerOnChange(() => res(manager.getBaseSchema())));
-    manager.startWatch();
-    await expect(lazySchema).resolves.toBeInstanceOf(GraphQLSchema);
-  });
-
-  it('should wait for base schema from GraphQL server', async () => {
-    server.use(schemaHandler);
-    const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
-    const manager = new HttpSchemaManager(schemaManagerHost, {
-      method: 'POST',
-      url: 'http://localhost/graphql',
-    });
-    const schema = await manager.waitBaseSchema();
-    expect(schema).toBeInstanceOf(GraphQLSchema);
-  });
-
-  it('should retry to fetch', async () => {
-    const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
-    const manager = new HttpSchemaManager(schemaManagerHost, {
-      method: 'POST',
-      url: 'http://localhost/graphql',
+  describe(HttpSchemaManager.prototype.startWatch, () => {
+    it('should get schema from GraphQL server', async () => {
+      server.use(schemaHandler);
+      const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
+      const manager = new HttpSchemaManager(schemaManagerHost, {
+        method: 'POST',
+        url: 'http://localhost/graphql',
+      });
+      const lazySchema = new Promise(res => manager.registerOnChange(() => res(manager.getBaseSchema())));
+      manager.startWatch();
+      await expect(lazySchema).resolves.toBeInstanceOf(GraphQLSchema);
     });
 
-    server.use(http.post('http://localhost/graphql', () => new Response(null, { status: 401 }), { once: true }));
+    it('should retry to fetch until GraphQL server responds valid introspection result', async () => {
+      const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
+      const manager = new HttpSchemaManager(schemaManagerHost, {
+        method: 'POST',
+        url: 'http://localhost/graphql',
+      });
 
-    manager.startWatch(50);
-    await setTimeout(50);
+      server.use(http.post('http://localhost/graphql', () => new Response(null, { status: 401 }), { once: true }));
 
-    expect(manager.getBaseSchema()).toBeNull();
-    server.use(schemaHandler);
+      manager.startWatch(50);
+      await setTimeout(50);
 
-    await setTimeout(32);
-    const schema = await manager.waitBaseSchema();
+      expect(manager.getBaseSchema()).toBeNull();
+      server.use(schemaHandler);
 
-    expect(schema).toBeInstanceOf(GraphQLSchema);
+      await setTimeout(32);
+      const schema = await manager.waitBaseSchema();
+
+      expect(schema).toBeInstanceOf(GraphQLSchema);
+    });
+
+    it('should retry to fetch unless _getOptions throw error', async () => {
+      class TestingHttpSchemaManager extends HttpSchemaManager {
+        private reqCount = 0;
+        async _getOptions() {
+          if (this.reqCount++ === 0) {
+            return null;
+          } else {
+            return {
+              url: 'http://localhost/graphql',
+            };
+          }
+        }
+      }
+      const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
+      const manager = new TestingHttpSchemaManager(schemaManagerHost);
+
+      server.use(schemaHandler);
+
+      manager.startWatch(50);
+      await setTimeout(50);
+
+      expect(manager.getBaseSchema()).toBeNull();
+
+      await setTimeout(32);
+      const schema = await manager.waitBaseSchema();
+
+      expect(schema).toBeInstanceOf(GraphQLSchema);
+    });
+
+    it('should retry to fetch _getOptions returns null', async () => {
+      class TestingHttpSchemaManager extends HttpSchemaManager {
+        private reqCount = 0;
+        async _getOptions() {
+          if (this.reqCount++ === 0) {
+            throw new Error();
+          } else {
+            return {
+              url: 'http://localhost/graphql',
+            };
+          }
+        }
+      }
+      const schemaManagerHost = createTestingSchemaManagerHost({ schema: '' });
+      const manager = new TestingHttpSchemaManager(schemaManagerHost);
+
+      server.use(schemaHandler);
+
+      manager.startWatch(50);
+      await setTimeout(50);
+
+      expect(manager.getBaseSchema()).toBeNull();
+
+      await setTimeout(32);
+      const schema = await manager.waitBaseSchema();
+
+      expect(schema).toBeInstanceOf(GraphQLSchema);
+    });
   });
 });
